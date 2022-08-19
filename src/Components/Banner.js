063ea980e2ea39acf89912f21fd384937dd8c82a
Eshratjahan17 <eshratjahan701@gmail.com>
2022-08-20 01:23:49 +0600

    banner

---
+++ src/Components/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import banner from '../assests/Images/banner.jpg';
+
+const Banner = () => {
+  return (
+    <div>
+      <div></div>
+      <img src={banner} alt="" />
+    </div>
+  );
+};
+
+export default Banner;
